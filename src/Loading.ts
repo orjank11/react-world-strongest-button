0899861e3757aed06a4bb117ff079f991438235f
orjank11 <orjank11@users.noreply.github.com>
2018-09-14 11:42:20 +0200

    Trying and failing animations

--- src/Loading.ts
+++ src/Loading.ts
@@ -1,103 +1,4 @@
-import * as cn from 'classnames';
-import * as React from 'react';
-
-import './Button.css';
-// import { outerAnimation, reset } from './loading';
-// import Loading from './Loading';
-
-type ButtonType = 'default' | 'primary' | 'danger' | 'dashed' | 'transparent';
-type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-interface IDropdownProps {
-  href: string;
-  label: string;
-  icon: string;
-}
-
-interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  isLoading?: boolean;
-  isSuccess?: boolean;
-  dropdown?: IDropdownProps[];
-  modal?: React.ReactNode;
-  size?: ButtonSize;
-  type?: ButtonType;
-  failure?: string | boolean;
-}
-
-interface IButtonState {
-  isDropdown?: boolean;
-  isLoading?: boolean;
-  isModal?: boolean;
-  isSubmitButton?: boolean;
-  isSuccess?: boolean;
-  isFailure?: boolean;
-  isFocused?: boolean;
-}
-
-class App extends React.Component<IButtonProps, IButtonState> {
-  constructor(props: IButtonProps) {
-    super(props);
-
-    const customProperties = {
-      isDropdown: !!props.dropdown,
-      isFailure: !!props.failure,
-      isLoading: props.isLoading,
-      isModal: !!props.modal,
-      isSubmitButton: !!props.onSubmit,
-      isSuccess: props.isSuccess,
-    };
-    this.setState({ ...customProperties });
-  }
-
-  // public Loading = Loading;
-
-  public render() {
-    return (
-      <>
-        <button
-          onClick={this.messageRoot}
-          className={cn("wsb-button", this.state && {
-          })}
-          onFocus={this.focus}
-          onBlur={this.blur}
-        >
-          <svg width="120" height="120" fill="rgba(0,0,0,0)">
-            <path className="outer-path" stroke="rgba(255, 255, 255, 0.85)" d="M 60 60 m 0 -50 a 50 50 0 1 1 0 100 a 50 50 0 1 1 0 -100" />
-            <path className="inner-path" stroke="rgba(255, 255, 255, 0.45)" d="M 60 60 m 0 -30 a 30 30 0 1 1 0 60 a 30 30 0 1 1 0 -60" />
-            <path className="success-path" stroke="rgba(255, 255, 255, 0.85)" d="M 60 10 A 50 50 0 0 1 91 21 L 75 45 L 55 75 L 45 65" />
-            <path className="error-path" stroke="rgba(255, 255, 255, 0.85)" d="M 60 10 A 50 50 0 0 1 95 25 L 45 75" />
-            <path className="error-path2" stroke="rgba(255, 255, 255, 0.85)" d="M 60 30 A 30 30 0 0 1 81 81 L 45 45" />
-          </svg>
-          {this.props.children}
-        </button>
-        {this.state && this.state.isModal && (
-          <>
-            <div className="wsb-modal__container">{this.props.modal}</div>
-            <div className="wsb-modal__out-side" />
-          </>
-        )}
-      </>
-    );
-  }
-
-  private blur = (event: any) => {
-
-    this.setState({ isFocused: true });
-    if (this.props && this.props.onBlur) {
-      return this.props.onBlur(event);
-    }
-  }
-  private focus = (foo: React.FocusEvent<HTMLButtonElement>) => {
-    this.setState({ isFocused: true });
-    if (this.props && this.props.onFocus) {
-      return this.props.onFocus(foo);
-    }
-  }
-  private messageRoot = () => {
-    this.reset();
-    document.querySelector('svg')!.style.visibility = 'visible';
-    document.getElementById('root')!.innerHTML = `<p>Hello</p> ${document.getElementById('root')!.innerHTML}`
-  }
-
+class Loading {
   public outer: any = document.querySelector('.outer-path');
   public inner: any = document.querySelector('.inner-path');
   public success: any = document.querySelector('.success-path');
@@ -182,7 +83,7 @@ class App extends React.Component<IButtonProps, IButtonState> {
       var timing = (options.timing) ? options.timing : 'linear';
       el.style.transition = el.style.WebkitTransition = 'none';
 
-      el.style.transition = el.style.WebkitTransition = 'stroke-dashoffset ' + options.duration + 's ' + timing + ', stroke-dasharray ' + options.duration + 's ' + timing;
+      el.style.transition = el.style.WebkitTransition =  'stroke-dashoffset ' + options.duration + 's ' + timing + ', stroke-dasharray ' + options.duration + 's ' + timing;
       setTimeout(() => { this.setupPath(el, length, options.start, options.end, opacity); });
     }
   }
@@ -198,26 +99,24 @@ class App extends React.Component<IButtonProps, IButtonState> {
     return this;
   }
 
-  outerAnimation = (outerCircle: any) => {
-    console.log(this.outerCircle);
-
-    outerCircle.set({
+  outerAnimation = () => {
+    this.outerCircle.set({
       start: '15%',
       end: '25%',
       duration: 0.2,
       callback: function () {
-        outerCircle.set({
+        this.outerCircle.set({
           start: '75%',
           end: '150%',
           duration: 0.3,
           follow: true,
           callback: function () {
-            outerCircle.set({
+            this.outerCircle.set({
               start: '70%',
               end: '75%',
               duration: 0.3,
               callback: function () {
-                outerCircle.set({
+                this.outerCircle.set({
                   start: '100%',
                   end: '100.1%',
                   duration: 0.4,
@@ -237,8 +136,8 @@ class App extends React.Component<IButtonProps, IButtonState> {
                       this.errorAnimation();
                     }
                     else {
-                      this.outerAnimation(outerCircle);
-                      // this.innerAnimation();
+                      this.outerAnimation();
+                      this.innerAnimation();
                     }
                   }
                 })
@@ -268,11 +167,6 @@ class App extends React.Component<IButtonProps, IButtonState> {
   }
 
   public reset = () => {
-    this.outer = document.querySelector('.outer-path');
-    this.inner = document.querySelector('.inner-path');
-    this.success = document.querySelector('.success-path');
-    this.error = document.querySelector('.error-path');
-    this.error2 = document.querySelector('.error-path2');
     this.success!.style.visibility = 'hidden';
     this.error!.style.visibility = 'hidden';
     this.error2!.style.visibility = 'hidden';
@@ -284,10 +178,10 @@ class App extends React.Component<IButtonProps, IButtonState> {
     this.outer.style.visibility = 'visible';
     this.inner.style.visibility = 'visible';
     setTimeout(() => {
-      this.outerAnimation(this.setPosition(this.outer, { start: '0%', end: '0.1%' }));
-      // this.innerAnimation(this.setPosition(this.inner, { start: '0%', end: '0.1%' }));
+      this.outerAnimation();
+      this.innerAnimation();
     });
   }
 }
 
-export default App;
+export default Loading;
